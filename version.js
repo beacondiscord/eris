bee1ecbbf47912d1a0bd36ad7b68c2a436b5c302
Jonathan <54381371+axisiscool@users.noreply.github.com>
2021-10-18 13:49:02 -0400

    refactor: remove version generator file

--- version.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const package = require('./package.json');
-
-package.version = `0.16.0-${Date.now()}`;
-
-fs.writeFileSync('./package.json', JSON.stringify(package, null, 2));
-
-console.log(`[Version Updater]: Bumped to ${package.version}`);
